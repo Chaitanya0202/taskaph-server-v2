ecbd5f9e11215ef9246da20128a2042a95c343ca
Chaitanya0202 <115619207+Chaitanya0202@users.noreply.github.com>
2024-12-16 23:01:25 +0530

    first commit

--- src/main/java/com/apixelhouse/taskaph/configuration/WebConfig.java
+++ src/main/java/com/apixelhouse/taskaph/configuration/WebConfig.java
@@ -7,8 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	  @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**").allowedOrigins("http://localhost:5173/") 
-	                .allowedMethods("*");
-	    }	
+//	    public void addCorsMappings(CorsRegistry registry) {
+//	        registry.addMapping("/**").allowedOrigins("*") 
+//	                .allowedMethods("*");
+//	    }
+	  public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**") // Allow all paths
+	                .allowedOrigins("*") // Allow all origins
+	                .allowedMethods("*"); // Allow all HTTP methods
+	    }
 }
